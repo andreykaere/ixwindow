78c0df3e698d8bfe77c9d41198190d6e405fb4e2
Andrey <andreykaere@gmail.com>
2023-02-08 20:59:58 +0300

    Add `ixwindow-convert` util

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ PREFIX="$HOME/.config/polybar/scripts/ixwindow"
 # Folder for cached icons
 CACHE="$HOME/.config/polybar/scripts/ixwindow/polybar-icons"
 
+# Folder for config icons
+CONFIG_DIR="$HOME/.config/ixwindow"
+CONFIG_FILE="$CONFIG_DIR/config.toml"
+
 # Size of the icon
 SIZE=24
 
@@ -18,11 +22,22 @@ X=270
 Y=6
 
 
+generate_config () {
+    local content="\
+size = $SIZE
+color = \"$COLOR\"\
+"
+    echo "$content" > "$CONFIG_FILE"
+}
+
 
 cp -R ixwindow ixwindow_compiled
 
 mkdir -p "$CACHE"
 mkdir -p "$PREFIX"
+mkdir -p "$CONFIG_DIR"
+
+generate_config
 
 
 sed -i "s/\$X/$X/g" ixwindow_compiled/polybar-xwindow-icon.cpp
@@ -35,10 +50,12 @@ sed -i "s/\$\$COLOR/\"$COLOR\"/g" ixwindow_compiled/generate-icon
 
 CACHE="$(echo "$CACHE" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 DIR="$(echo "$PREFIX" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
+CONFIG_FILE="$(echo "$CONFIG_FILE" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
 
 sed -i "s/\$\$CACHE/\"$CACHE\"/g" ixwindow_compiled/ixwindow
 sed -i "s/\$\$DIR/\"$DIR\"/g" ixwindow_compiled/ixwindow
+sed -i "s/\$\$CONFIG/\"$CONFIG_FILE\"/g" ixwindow_compiled/ixwindow-convert
 
 
 g++ ixwindow_compiled/polybar-xwindow-icon.cpp -o ixwindow_compiled/polybar-xwindow-icon -I/usr/include/opencv4/ -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lX11
