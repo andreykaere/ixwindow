1f259c424ef80cfdc9da08644d941b5cd390e694
Andrey <andreykaere@gmail.com>
2023-02-09 17:44:53 +0300

    Adjust install.sh and generate-icon to new scheme

--- install.sh
+++ install.sh
@@ -15,16 +15,6 @@ mkdir -p "$CONFIG_DIR"
 generate_config
 
 
-sed -i "s/\$X/$X/g" ixwindow_compiled/polybar-xwindow-icon.cpp
-sed -i "s/\$Y/$Y/g" ixwindow_compiled/polybar-xwindow-icon.cpp
-sed -i "s/\$SIZE/$SIZE/g" ixwindow_compiled/polybar-xwindow-icon.cpp
-
-
-sed -i "s/\$\$SIZE/$SIZE/g" ixwindow_compiled/generate-icon
-sed -i "s/\$\$COLOR/\"$COLOR\"/g" ixwindow_compiled/generate-icon
-
-CACHE="$(echo "$CACHE" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
-DIR="$(echo "$PREFIX" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 CONFIG_FILE="$(echo "$CONFIG_FILE" | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
 
@@ -32,14 +22,10 @@ sed -i "s/\$\$CONFIG/\"$CONFIG_FILE\"/g" ixwindow_compiled/ixwindow-convert
 
 case "$WM"
     "bspwm")
-        sed -i "s/\$\$CACHE/\"$CACHE\"/g" ixwindow_compiled/bspwm/ixwindow
-        sed -i "s/\$\$DIR/\"$DIR\"/g" ixwindow_compiled/bspwm/ixwindow
 
         rm -r "ixwindow_compiled/i3"
         ;;
     "i3")
-        # sed -i "s/\$\$CACHE/\"$CACHE\"/g" ixwindow_compiled/i3/ixwindow
-        # sed -i "s/\$\$DIR/\"$DIR\"/g" ixwindow_compiled/i3/ixwindow
         
         rm -r "ixwindow_compiled/bspwm"
         ;;
