2793926262baca209ffd6ead9a6c912fb0966c65
Andrey <andreykaere@gmail.com>
2023-02-15 00:32:48 +0300

    Add types to signatures, minor refactoring

--- ixwindow/wms/i3/src/main.rs
+++ ixwindow/wms/i3/src/main.rs
@@ -1,13 +1,15 @@
-use i3ipc::event::Event;
+use i3ipc::event::{
+    inner::WindowChange, Event, ModeEventInfo, WindowEventInfo,
+    WorkspaceEventInfo,
+};
 use i3ipc::Subscription;
 use i3ipc::{self, I3Connection, I3EventListener};
+
 use std::error::Error;
+use std::path::Path;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // establish a connection to i3 over a unix socket
-    // let mut connection = I3Connection::connect().unwrap();
-
-    let mut listener = I3EventListener::connect()?;
+fn main() {
+    let mut listener = I3EventListener::connect().unwrap();
 
     let subscriptions = [
         Subscription::Workspace,
@@ -18,20 +20,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     listener.subscribe(&subscriptions);
 
     for event in listener.listen() {
-        // let y: i32 = event;
-
         match event {
             Ok(res) => {
                 handle_event(res);
             }
 
             Err(e) => {
-                println!("Encounter the following error: {e}");
+                println!("While listening to events, encounter the following error: {e}");
             }
         }
     }
-
-    Ok(())
 }
 
 fn handle_event(event: Event) {
@@ -41,17 +39,19 @@ fn handle_event(event: Event) {
         Event::WindowEvent(e) => handle_window_event(e),
         Event::WorkspaceEvent(e) => handle_workspace_event(e),
         Event::ModeEvent(e) => handle_mode_event(e),
+        _ => {}
     }
 }
 
 fn handle_window_event(event: WindowEventInfo) {
-    let node = event.node;
+    let node = event.container;
 
     match event.change {
         WindowChange::New => {}
         WindowChange::Close => {}
         WindowChange::Focus => {}
         WindowChange::FullscreenMode => {}
+        _ => {}
     }
 }
 
@@ -59,23 +59,21 @@ fn handle_workspace_event(event: WorkspaceEventInfo) {}
 
 fn handle_mode_event(event: ModeEventInfo) {}
 
+fn generate_icon() {}
 
-
-fn generate_icon() {
+fn display_icon(
+    path: &Path,
+    cache_dir: &Path,
+    size: u8,
+    color: &str,
+    name: &str,
+) {
 }
 
-fn display_icon(path: , cache_dir: , size: , color: , name: ) {
-}
+fn print_info(gap: &str, info: &str) {}
 
-fn print_info(gap: , info: &str) {
-}
+fn cleanup_icon() {}
 
-fn cleanup_icon() {
-}
+fn parse_config() {}
 
-fn parse_config() {
-}
-
-
-fn cache_curr_icon() {
-}
+fn cache_curr_icon() {}
