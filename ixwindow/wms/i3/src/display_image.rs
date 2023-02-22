470624a287f22aff75cc8b8b7a362514bb504397
Andrey <andreykaere@gmail.com>
2023-02-22 14:04:50 +0300

    Get display_image work, but background is black

--- ixwindow/wms/i3/src/display_image.rs
+++ ixwindow/wms/i3/src/display_image.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use image::imageops::FilterType;
 use image::io::Reader;
 use image::GenericImageView;
 use x11rb::connection::Connection;
@@ -13,13 +14,15 @@ pub fn display_image(
     size: u16,
 ) -> Result<(), Box<dyn Error>> {
     let image = Reader::open(image_path)?.decode()?;
+    // let image = image.resize(size as u32, size as u32, FilterType::CatmullRom);
     let (width, height) = image.dimensions();
 
     let (conn, screen_num) = x11rb::connect(None)?;
     let screen = &conn.setup().roots[screen_num];
 
     let win = conn.generate_id()?;
-    let window_aux = CreateWindowAux::new()
+    let window_aux = CreateWindowAux::default()
+        .override_redirect(1)
         .border_pixel(screen.black_pixel)
         .event_mask(EventMask::EXPOSURE);
 
@@ -37,53 +40,82 @@ pub fn display_image(
         screen.root_visual,
         &window_aux,
     )?;
+    conn.map_window(win)?;
+    conn.flush()?;
 
     let gc_aux = CreateGCAux::new();
     let gc = conn.generate_id()?;
     create_gc(&conn, gc, win, &gc_aux)?;
 
-    let pixmap = conn.generate_id()?;
-    conn.create_pixmap(
+    // let image = image::open("path/to/image.png")?.to_rgba8();
+    // let (width, height) = image.dimensions();
+    let (pixmap, pixmap_bytes) = x11rb::image::shm::create_pixmap(
+        &conn,
+        screen.root,
+        width,
+        height,
         screen.root_depth,
-        pixmap,
-        win,
-        width as u16,
-        height as u16,
     )?;
 
-    let data = image.into_rgba8().into_raw();
-    conn.put_image(
-        ImageFormat::Z_PIXMAP,
-        pixmap,
-        gc,
-        width as u16,
-        height as u16,
-        0,
-        0,
-        0,
-        screen.root_depth,
-        &data,
-    )?;
+    let mut pixmap_data = pixmap_bytes.map()?;
+    let stride = pixmap_data.stride();
+    let mut offset = 0;
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = image.get_pixel(x, y);
+            pixmap_data[offset] = pixel[2];
+            pixmap_data[offset + 1] = pixel[1];
+            pixmap_data[offset + 2] = pixel[0];
+            pixmap_data[offset + 3] = pixel[3];
+            offset += 4;
+        }
+        offset += stride - width * 4;
+    }
 
-    conn.copy_area(
-        pixmap,
-        win,
-        gc,
-        0,
-        0,
-        0,
-        0,
-        width.try_into()?,
-        height.try_into()?,
-    )?;
+    // let pixmap = conn.generate_id()?;
+    // conn.create_pixmap(
+    //     screen.root_depth,
+    //     pixmap,
+    //     win,
+    //     width as u16,
+    //     height as u16,
+    // )?;
 
-    conn.map_window(win)?;
-    conn.flush()?;
+    // let mut data = image.into_rgba8().into_raw();
+    // data.chunks_exact_mut(4).for_each(|chunk| {
+    //     let (c0, c2) = (chunk[0], chunk[2]);
+    //     chunk[2] = c0;
+    //     chunk[0] = c2;
+    // });
+
+    // conn.put_image(
+    //     ImageFormat::Z_PIXMAP,
+    //     pixmap,
+    //     gc,
+    //     width as u16,
+    //     height as u16,
+    //     0,
+    //     0,
+    //     0,
+    //     screen.root_depth,
+    //     &data,
+    // )?;
 
     loop {
         let event = conn.wait_for_event()?;
 
         if let Event::Expose(_) = event {
+            conn.copy_area(
+                pixmap,
+                win,
+                gc,
+                0,
+                0,
+                0,
+                0,
+                width.try_into()?,
+                height.try_into()?,
+            )?;
             conn.flush()?;
         }
     }
