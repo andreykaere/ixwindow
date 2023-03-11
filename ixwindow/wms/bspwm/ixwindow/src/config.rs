a9683d69504f7d8c8eea9d108a2241ae045548bf
Andrey <andreykaere@gmail.com>
2023-03-11 15:28:05 +0300

    Update config for bspwm

--- ixwindow/wms/bspwm/ixwindow/src/config.rs
+++ ixwindow/wms/bspwm/ixwindow/src/config.rs
@@ -1,12 +1,11 @@
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use toml::Value;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct I3Config {
+pub struct EssentialConfig {
     pub prefix: String,
     pub gap: String,
     pub x: i16,
@@ -14,120 +13,149 @@ pub struct I3Config {
     pub size: u16,
     pub cache_dir: String,
     pub color: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct I3Config {
+    #[serde(flatten)]
+    pub essential_config: EssentialConfig,
+
     pub gap_per_desk: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BspwmConfig {
-    pub prefix: String,
-    pub gap: String,
-    pub x: u16,
-    pub y: u16,
-    pub size: u16,
-    pub cache_dir: String,
-    pub color: String,
+    #[serde(flatten)]
+    pub essential_config: EssentialConfig,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
-    #[serde(rename = "i3wm")]
-    #[serde(deserialize_with = "ok_or_default")]
-    i3: Option<I3Config>,
+pub trait Config {
+    fn essential_config(&self) -> &EssentialConfig;
 
-    #[serde(deserialize_with = "ok_or_default")]
-    bspwm: Option<BspwmConfig>,
+    fn gap(&self) -> &str {
+        &self.essential_config().gap
+    }
+
+    fn color(&self) -> &str {
+        &self.essential_config().color
+    }
+
+    fn cache_dir(&self) -> &str {
+        &self.essential_config().cache_dir
+    }
+
+    fn prefix(&self) -> &str {
+        &self.essential_config().prefix
+    }
+
+    fn x(&self) -> i16 {
+        self.essential_config().x
+    }
+
+    fn y(&self) -> i16 {
+        self.essential_config().y
+    }
+
+    fn size(&self) -> u16 {
+        self.essential_config().size
+    }
 }
 
-// We don't want to panic in case one of the config is wrong, because it
-// should not be related to another one, since they do not depend on each
-// other
-// Copied from https://users.rust-lang.org/t/solved-serde-deserialization-on-error-use-default-values/6681
-fn ok_or_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
-where
-    T: Deserialize<'de> + Default,
-    D: Deserializer<'de>,
-{
-    let v: Value = Deserialize::deserialize(deserializer).unwrap();
-    Ok(T::deserialize(v).unwrap_or_default())
+impl Config for I3Config {
+    fn essential_config(&self) -> &EssentialConfig {
+        &self.essential_config
+    }
 }
 
-impl Config {
-    pub fn init() -> Self {
-        let config_filename = match Self::locate_config_file() {
-            Some(config_file) => config_file,
-            None => {
-                if let Some(config_opt) = Self::process_config_as_option() {
-                    config_opt
-                } else {
-                    panic!("Couldn't find config file");
-                }
-            }
-        };
-
-        let mut config_file =
-            File::open(config_filename).expect("Failed to open config file");
-        let mut config_str = String::new();
-        config_file.read_to_string(&mut config_str).unwrap();
-        let config: Config = toml::from_str(&config_str).unwrap();
-
-        config
+impl Config for BspwmConfig {
+    fn essential_config(&self) -> &EssentialConfig {
+        &self.essential_config
     }
+}
 
-    pub fn load_i3() -> I3Config {
-        let config = Self::init();
-        let mut i3_config =
-            config.i3.expect("While parsing config error occured");
-        i3_config.cache_dir = expand_filename(&i3_config.cache_dir);
-        i3_config.prefix = expand_filename(&i3_config.prefix);
-
-        i3_config
-    }
-
-    pub fn load_bspwm() -> BspwmConfig {
-        let config = Self::init();
-        let mut bspwm_config =
-            config.bspwm.expect("While parsing config error occured");
-        bspwm_config.cache_dir = expand_filename(&bspwm_config.cache_dir);
-        bspwm_config.prefix = expand_filename(&bspwm_config.prefix);
-
-        bspwm_config
-    }
-
-    pub fn process_config_as_option() -> Option<String> {
-        let args = env::args();
-
-        for arg in args {
-            let parse: Vec<_> = arg.split("--config=").collect();
-
-            if parse.len() == 2 {
-                return Some(parse[1].to_string());
+pub fn read_to_table() -> toml::Table {
+    let config_filename = match locate_config_file() {
+        Some(config_file) => config_file,
+        None => {
+            if let Some(config_opt) = process_config_as_option() {
+                config_opt
+            } else {
+                panic!("Couldn't find config file");
             }
         }
+    };
 
-        None
-    }
+    let mut config_file =
+        File::open(config_filename).expect("Failed to open config file");
+    let mut config_str = String::new();
+    config_file.read_to_string(&mut config_str).unwrap();
 
-    pub fn locate_config_file() -> Option<String> {
-        if let Ok(default_dir) = env::var("XDG_CONFIG_HOME") {
-            let default_config =
-                format!("{default_dir}/ixwindow/ixwindow.toml");
-
-            if Path::new(&default_config).exists() {
-                return Some(default_config);
-            }
-        }
-
-        if let Ok(specified_config) = env::var("IXWINDOW_CONFIG_PATH") {
-            if Path::new(&specified_config).exists() {
-                return Some(specified_config);
-            }
-        }
-
-        None
-    }
+    config_str.parse().unwrap()
 }
 
-pub fn expand_filename(filename: &str) -> String {
+pub fn load_i3() -> I3Config {
+    let mut table = read_to_table();
+
+    // We use remove here, because we need ownership for try_into
+    let config_table = table.remove("i3").unwrap();
+
+    let mut i3_config: I3Config = config_table.try_into().unwrap();
+    i3_config.essential_config.cache_dir =
+        expand_filename(&i3_config.essential_config.cache_dir);
+    i3_config.essential_config.prefix =
+        expand_filename(&i3_config.essential_config.prefix);
+
+    i3_config
+}
+
+pub fn load_bspwm() -> BspwmConfig {
+    let mut table = read_to_table();
+
+    // We use remove here, because we need ownership for try_into
+    let config_table = table.remove("bspwm").unwrap();
+
+    let mut bspwm_config: BspwmConfig = config_table.try_into().unwrap();
+    bspwm_config.essential_config.cache_dir =
+        expand_filename(&bspwm_config.essential_config.cache_dir);
+    bspwm_config.essential_config.prefix =
+        expand_filename(&bspwm_config.essential_config.prefix);
+
+    bspwm_config
+}
+
+fn process_config_as_option() -> Option<String> {
+    let args = env::args();
+
+    for arg in args {
+        let parse: Vec<_> = arg.split("--config=").collect();
+
+        if parse.len() == 2 {
+            return Some(parse[1].to_string());
+        }
+    }
+
+    None
+}
+
+fn locate_config_file() -> Option<String> {
+    if let Ok(default_dir) = env::var("XDG_CONFIG_HOME") {
+        let default_config = format!("{default_dir}/ixwindow/ixwindow.toml");
+
+        if Path::new(&default_config).exists() {
+            return Some(default_config);
+        }
+    }
+
+    if let Ok(specified_config) = env::var("IXWINDOW_CONFIG_PATH") {
+        if Path::new(&specified_config).exists() {
+            return Some(specified_config);
+        }
+    }
+
+    None
+}
+
+fn expand_filename(filename: &str) -> String {
     let filename = &shellexpand::env(filename).unwrap();
     let filename = shellexpand::tilde(filename).to_string();
 
@@ -140,32 +168,32 @@ mod tests {
 
     #[test]
     fn locate_config_file_works() {
-        Config::locate_config_file();
+        locate_config_file();
     }
 
     #[test]
     fn parse_config_works() {
-        let config = Config::load_i3();
+        let config = load_i3();
 
-        assert_eq!(config.size, 24);
+        assert_eq!(config.size(), 24);
         assert_eq!(
-            config.cache_dir,
+            config.cache_dir(),
             "/home/andrey/.config/polybar/scripts/ixwindow/polybar-icons"
         );
     }
 
     #[test]
     fn expand_filename_works() {
-        let config = Config::load_i3();
+        let config = load_i3();
 
         assert_eq!(
-            expand_filename(&config.cache_dir),
+            expand_filename(config.cache_dir()),
             "/home/andrey/.config/polybar/scripts/ixwindow/polybar-icons"
         );
     }
 
     #[test]
     fn process_config_as_option_works() {
-        Config::process_config_as_option();
+        process_config_as_option();
     }
 }
--- ixwindow/wms/bspwm/ixwindow/src/main.rs
+++ ixwindow/wms/bspwm/ixwindow/src/main.rs
@@ -1,20 +1,21 @@
 use std::process::{Command, Stdio};
 
 mod config;
+
 use config::Config;
 
 fn main() {
-    let config = Config::load_bspwm();
+    let config = config::load_bspwm();
 
     let mut child =
-        Command::new(format!("{}/bspwm/ixwindow.sh", &config.prefix))
-            .arg(&config.prefix)
-            .arg(&config.cache_dir)
-            .arg(&config.gap)
-            .arg(config.x.to_string())
-            .arg(config.y.to_string())
-            .arg(config.size.to_string())
-            .arg(&config.color)
+        Command::new(format!("{}/bspwm/ixwindow.sh", config.prefix()))
+            .arg(config.prefix())
+            .arg(config.cache_dir())
+            .arg(config.gap())
+            .arg(config.x().to_string())
+            .arg(config.y().to_string())
+            .arg(config.size().to_string())
+            .arg(config.color())
             .stderr(Stdio::null())
             .spawn()
             .unwrap();
